001878dc64e8f7a75539ecf373bd20dc8e44edd2
Mitchell Gillin <mtg4ra@virginia.edu>
2018-04-25 11:34:20 -0400

    Fix layout

--- msmcapstone/src/Ux2Wrapper.js
+++ msmcapstone/src/Ux2Wrapper.js
@@ -2,6 +2,7 @@ import React from "react"
 import Ux2 from "/Users/mitchellgillin/Desktop/msmcapstonespring/msmcapstone/src/Ux2.js"
 import Ux2Generator from "./Ux2Generator.js"
 import MyHeader from "./MyHeader.js";
+import { Row, Col } from "antd"
 
 export default class UX2Wrapper extends React.Component {
   constructor(props) {
@@ -27,12 +28,17 @@ export default class UX2Wrapper extends React.Component {
       <div>
         <MyHeader />
         <Ux2Generator addDrugToArray={(obj) => this.addDrugToArray(obj)} />
-        {this.state.ux2array.map((treatment) =>
-          <Ux2
-            name={treatment.name}
-            dailyDose={treatment.dailyDose}
-            completed={treatment.completed} />)}
+        <Row gutter={12}>
+          {this.state.ux2array.map((treatment) =>
+            <Col span={8}>
+              <Ux2
+                name={treatment.name}
+                dailyDose={treatment.dailyDose}
+                completed={treatment.completed} />
+            </Col>
+          )}
 
+        </Row>
       </div>
     )
   }
